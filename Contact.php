954dda5423c1fb4173847514e88f50132a67c0bd
Gigic337 <124678071+Gigic337@users.noreply.github.com>
2023-03-25 21:47:36 +0100

    Vérification des champs, quete2 formulaire

--- Contact.php
+++ Contact.php
@@ -15,52 +15,77 @@
     </style>
 </head>
 <body>
-    <h2> Laissez nous un message</h2>
-    <p> Merci de remplir le formulaire ci-dessous pour nous contacter.</p>
+<h2> Laissez nous un message</h2>
+<p> Merci de remplir le formulaire ci-dessous pour nous contacter.</p>
+<form action="thanks.php" method="post">
 
-    <form action="thanks.php" method="post">
-        <p>
-            <label for="firstname"> Prénom </label>
-            <input type="text" id="firstname" name="firstname">
-        </p>
 
-        <p>
-            <label for="lastname"> Nom </label>
-            <input type="text" id="lastname" name="lastname">
-        </p>
+<?php
+            if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+                $errors = [];
+                if (!isset($_POST['firstname']) || trim($_POST['firstname']) === '') {
+                    $errors[] = "Le prénom est obligatoire";
+                }
+                if (!isset($_POST['lastname']) || trim($_POST['lastname']) === '') {
+                    $errors[] = "Le nom est obligatoire";
+                }
+                if (!isset($_POST['email']) || trim($_POST['email']) === '') {
+                    $errors[] = "L'e-mail est obligatoire";
+                }
+                if (!isset($_POST['phone']) || trim($_POST['phone']) === '') {
+                    $errors[] = "Le numéro de téléphone est obligatoire";
+                }
+                if (!isset($_POST['subject']) || trim($_POST['subject']) === '') {
+                    $errors[] = "Le sujet est obligatoire";
+                }
+                if (!isset($_POST['message']) || trim($_POST['message']) === '') {
+                    $errors[] = "Le message est obligatoire";
+                }
+                if (empty($errors)) {
+                    // Tous les champs sont remplis, le formulaire est valide
+                    header('Location: thanks.php');
+                    exit;
+                } else {
+                    // Au moins un champ est vide, on redirige l'utilisateur vers thanks.php avec les messages d'erreur
+                    $error_str = implode(',', $errors);
+                    header("Location: thanks.php?error=$error_str");
+                    exit;
+                }
+            }
+?>
 
-        <p>
-            <label for="mail"> E-mail</label>
-            <input type="email" id="mail" name="email">
-        </p>
-
-        <p>
-            <label for="phone"> Numéro de téléphone</label>
-            <input type="tel" id="phone" name="phone">
-        </p>
-
-        <p>
-            <label for="subject"> Vous nous contacter à propose de</label>
-            <select name="subject" id="subject">
+    <p>
+        <label for="firstname"> Prénom </label>
+        <input type="text" id="firstname" name="firstname">
+    </p>
+    <p>
+        <label for="lastname"> Nom </label>
+        <input type="text" id="lastname" name="lastname">
+    </p>
+    <p>
+        <label for="mail"> E-mail</label>
+        <input type="email" id="mail" name="email">
+    </p>
+    <p>
+        <label for="phone"> Numéro de téléphone</label>
+        <input type="tel" id="phone" name="phone">
+    </p>
+    <p>
+        <label for="subject"> Vous nous contacter à propose de</label>
+        <select name="subject" id="subject">
             <option value="subscription"> Votre souscription </option>
             <option value="options"> Votre option</option>
             <option value="appreciation"> Un avis sur notre site</option>
             <option value="another"> Autre Chose</option>
-            </select>
-        </p>
-
-        <p>
-            <label for="message"> Ecrivez votre message</label>
-            <textarea name="message" id="message"></textarea>
-        </p>
-
-        <p>
-            <button type="submit"> Validez votre message</button>
-        </p>
-
+        </select>
+    </p>
+    <p>
+        <label for="message"> Ecrivez votre message</label>
+        <textarea name="message" id="message"></textarea>
+    </p>
+    <p>
+        <button type="submit"> Validez votre message</button>
+    </p>
 </form>
-
-
-
 </body>
 </html>
--- thanks.php
+++ thanks.php
@@ -1,12 +1,3 @@
-<?php
-
-$firstname = $_POST['firstname'];
-$lastname = $_POST['lastname'];
-$mail = $_POST['mail'];
-$phone = $_POST['phone'];
-$subject = $_POST['subject'];
-$message = $_POST['message'];
-?>
 
 <!doctype html>
 <html lang="fr">
@@ -15,28 +6,64 @@ $message = $_POST['message'];
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title> Thanks </title>
-    <style>
-        p {
-           margin: 10px;
-        }
-    </style>
+    <title>Confirmation de message</title>
 </head>
 <body>
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {$errors = [];
 
-        <p> Merci <?= $firstname. ' '. $lastname ?> de nous avoir contacté à propos de
-        <?= $subject?> </p>
+    if (!isset($_POST['firstname']) || trim($_POST['firstname']) === '') {
+        $errors[] = "Le prénom est obligatoire";
+    }
+    if (!isset($_POST['lastname']) || trim($_POST['lastname']) === '') {
+        $errors[] = "Le nom est obligatoire";
+    }
+    if (!isset($_POST['email']) || trim($_POST['email']) === '') {
+        $errors[] = "L'adresse e-mail est obligatoire";
+    }
+    if (!isset($_POST['phone']) || trim($_POST['phone']) === '') {
+        $errors[] = "Le numéro de téléphone est obligatoire";
+    }
+    if (!isset($_POST['subject']) || trim($_POST['subject']) === '') {
+        $errors[] = "Le sujet est obligatoire";
+    }
+    if (!isset($_POST['message']) || trim($_POST['message']) === '') {
+        $errors[] = "Le message est obligatoire";
+    }
 
-        <p> Un de nos conseillers vous contactera soit à l'adresse <?= $mail ?>
-            ou par téléphone au <?= $phone ?> dans les plus brefs délais
+    if (empty($errors)) {
+        $firstname = $_POST['firstname'];
+        $lastname = $_POST['lastname'];
+        $email = $_POST['email'];
+        $phone = $_POST['phone'];
+        $subject = $_POST['subject'];
+        $message = $_POST['message'];
+
+
+
+        echo "<p> Merci $firstname. ' '. $lastname ? de nous avoir contacté à propos de
+         $subject?</p>
+
+        <p> Un de nos conseillers vous contactera soit à l'adresse  $email 
+            ou par téléphone au  $phone  dans les plus brefs délais
             pour traiter votre demande:
         </p>
 
-        <p> <?= $message ?> </p>
-
-    </div>
-
-
-
+        <p>  $message  </p>";
+        echo "<p>Merci de nous avoir contacté. Nous vous répondrons dans les plus brefs délais.</p>";
+    } else {
+        echo "<h2>Il y a des erreurs dans le formulaire :</h2>";
+        echo "<ul>";
+        foreach ($errors as $error) {
+            echo "<li>$error</li>";
+        }
+        echo "</ul>";
+        echo "<p>Veuillez remplir les champs obligatoires et essayer de nouveau.</p>";
+    }
+} else {
+    header('Location: contact.php');
+    exit;
+}
+?>
 </body>
 </html>
